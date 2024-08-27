0a72eb4d4e6f637642c61d59e21fff7a1a6da1ad
DauphalXir <3013875189@qq.com>
2024-08-27 12:34:45 +0800

    fix HungerRing bug on server

--- src/main/java/com/xir/NHUtilities/common/items/baubles/GluttonyRing.java
+++ src/main/java/com/xir/NHUtilities/common/items/baubles/GluttonyRing.java
@@ -7,7 +7,9 @@ import com.xir.NHUtilities.common.items.aItemCore.ItemBasic;
 
 import baubles.api.BaubleType;
 import baubles.api.IBauble;
+import cpw.mods.fml.common.Optional;
 
+@Optional.Interface(iface = "baubles.api.IBauble", modid = "Baubles")
 public class GluttonyRing extends ItemBasic implements IBauble {
 
     public GluttonyRing() {
@@ -18,31 +20,37 @@ public class GluttonyRing extends ItemBasic implements IBauble {
     }
 
     @Override
+    @Optional.Method(modid = "Baubles")
     public BaubleType getBaubleType(ItemStack itemstack) {
         return BaubleType.RING;
     }
 
     @Override
+    @Optional.Method(modid = "Baubles")
     public void onWornTick(ItemStack itemstack, EntityLivingBase player) {
 
     }
 
     @Override
+    @Optional.Method(modid = "Baubles")
     public void onEquipped(ItemStack itemstack, EntityLivingBase player) {
 
     }
 
     @Override
+    @Optional.Method(modid = "Baubles")
     public void onUnequipped(ItemStack itemstack, EntityLivingBase player) {
 
     }
 
     @Override
+    @Optional.Method(modid = "Baubles")
     public boolean canEquip(ItemStack itemstack, EntityLivingBase player) {
         return true;
     }
 
     @Override
+    @Optional.Method(modid = "Baubles")
     public boolean canUnequip(ItemStack itemstack, EntityLivingBase player) {
         return true;
     }
