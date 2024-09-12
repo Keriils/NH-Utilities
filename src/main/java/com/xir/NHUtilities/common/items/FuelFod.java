59f3e56c5854d006172c3de7cb3544e121e93e4b
DauphalXir <rmk123456@126.com>
2024-09-12 10:37:17 +0800

    fix & add

--- src/main/java/com/xir/NHUtilities/common/items/FuelFod.java
+++ src/main/java/com/xir/NHUtilities/common/items/FuelFod.java
@@ -2,11 +2,20 @@ package com.xir.NHUtilities.common.items;
 
 import net.minecraft.item.ItemStack;
 
+import com.xir.NHUtilities.common.items.aItemCore.ItemBasic;
 import com.xir.NHUtilities.common.items.itemFuelRod.DepletedFuelRod;
 import com.xir.NHUtilities.common.items.itemFuelRod.FuelRod;
 import com.xir.NHUtilities.utils.RegisterUtils;
 
 public class FuelFod {
+
+    public static ItemBasic radianceFuelRod;
+    public static ItemBasic radianceFuelRod_2;
+    public static ItemBasic radianceFuelRod_4;
+
+    public static ItemBasic ichorFuelRod;
+    public static ItemBasic ichorFuelRod_2;
+    public static ItemBasic ichorFuelRod_4;
 
     public static DepletedFuelRod depletedFuelRodExcitedRadianceUranium;
     public static DepletedFuelRod depletedFuelRodExcitedRadianceUranium_2;
@@ -25,6 +34,14 @@ public class FuelFod {
     public static FuelRod fuelRodSuperExcitedPrimordialPearlPlutonium_4;
 
     public static void fuelRodInit() {
+
+        radianceFuelRod = new ItemBasic("radianceFuelRod");
+        radianceFuelRod_2 = new ItemBasic("radianceFuelRod_2");
+        radianceFuelRod_4 = new ItemBasic("radianceFuelRod_4");
+
+        ichorFuelRod = new ItemBasic("ichorFuelRod");
+        ichorFuelRod_2 = new ItemBasic("ichorFuelRod_2");
+        ichorFuelRod_4 = new ItemBasic("ichorFuelRod_4");
 
         depletedFuelRodExcitedRadianceUranium = new DepletedFuelRod("depletedFuelRodExcitedRadianceUranium", 100);
         depletedFuelRodExcitedRadianceUranium_2 = new DepletedFuelRod("depletedFuelRodExcitedRadianceUranium_2", 200);
@@ -95,6 +112,14 @@ public class FuelFod {
             10000,
             1.3F,
             new ItemStack(depletedFuelRodSuperExcitedPrimordialPearlPlutonium_4, 1));
+
+        RegisterUtils.registerItem(radianceFuelRod);
+        RegisterUtils.registerItem(radianceFuelRod_2);
+        RegisterUtils.registerItem(radianceFuelRod_4);
+
+        RegisterUtils.registerItem(ichorFuelRod);
+        RegisterUtils.registerItem(ichorFuelRod_2);
+        RegisterUtils.registerItem(ichorFuelRod_4);
 
         RegisterUtils.registerItem(depletedFuelRodExcitedRadianceUranium);
         RegisterUtils.registerItem(depletedFuelRodExcitedRadianceUranium_2);
