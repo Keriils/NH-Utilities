483750dc878d45f464afd5c4c952d59d99f35e1d
DauphalXir <rmk123456@126.com>
2025-03-12 23:44:15 +0800

    hook-test

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,3 +63,33 @@ spotless {
         importOrder("java", "javax", "net", "org", "com")
     }
 }
+
+tasks.register<Copy>("initGitHooks") {
+    val scriptsFile = File(rootDir, "git-hooks")
+    val gitHooksDir = File(rootDir, ".git/hooks")
+
+    onlyIf {
+        if (!gitHooksDir.exists()) {
+            println("No .git/hooks directory found. Skipping hooks installation.")
+            false
+        } else {
+            true
+        }
+    }
+
+    from(scriptsFile) { include("**/*") }
+
+    filesMatching("**/*") {
+        filePermissions {
+            // 0b111_101_101
+            unix(493)
+        }
+    }
+    into(gitHooksDir)
+
+    doLast { println("Task : Inited git-hooks executed done.") }
+}
+
+tasks.build { dependsOn(tasks.spotlessApply) }
+
+tasks.prepareKotlinBuildScriptModel { dependsOn("initGitHooks") }
