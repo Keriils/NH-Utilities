79c3bc00010bfc9256ec05e702cbbdfd9c189ad5
DauphalXir <rmk123456@126.com>
2025-03-17 15:36:35 +0800

    use ker-plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,68 +1,9 @@
-import com.diffplug.gradle.spotless.BaseKotlinExtension
-import com.diffplug.gradle.spotless.FormatExtension
-
 plugins {
     id("com.gtnewhorizons.gtnhconvention")
-    id("com.diffplug.spotless") version "7.0.2"
+    id("online.keriils.plugins.spotless-wrapper") version "0.1.1"
 }
 
-fun FormatExtension.applyCommonFormatSteps() {
-    toggleOffOn()
-    trimTrailingWhitespace()
-    endWithNewline()
-}
-
-@Suppress("SpellCheckingInspection")
-fun BaseKotlinExtension.applyCustomKtfmtConfig() {
-    ktfmt("0.54").googleStyle().configure {
-        it.setMaxWidth(120)
-        it.setBlockIndent(4)
-        it.setContinuationIndent(4)
-        it.setRemoveUnusedImports(true)
-        it.setManageTrailingCommas(true)
-    }
-}
-
-spotless {
-    encoding("UTF-8")
-
-    format("misc") {
-        target(".gitignore")
-        leadingSpacesToTabs()
-        applyCommonFormatSteps()
-    }
-
-    java {
-        target("src/*/java/**/*.java", "src/*/scala/**/*.java")
-
-        formatAnnotations()
-        removeUnusedImports()
-        applyCommonFormatSteps()
-        importOrder("java", "javax", "net", "org", "com")
-        eclipse("4.19").configFile(file("SpotlessPlugin/spotless.eclipseFormat.xml"))
-    }
-
-    kotlin {
-        target("src/*/kotlin/**/*.kt")
-        leadingSpacesToTabs()
-        applyCustomKtfmtConfig()
-        applyCommonFormatSteps()
-    }
-
-    kotlinGradle {
-        target("*.gradle.kts")
-        applyCustomKtfmtConfig()
-        applyCommonFormatSteps()
-    }
-
-    groovyGradle {
-        target("*.gradle")
-
-        removeSemicolons()
-        applyCommonFormatSteps()
-        importOrder("java", "javax", "net", "org", "com")
-    }
-}
+wrapperSpotless { spotlessConfigGroovyGradle = false }
 
 tasks.register<Copy>("initGitHooks") {
     val scriptsFile = File(rootDir, "git-hooks")
