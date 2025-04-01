e7bd76f274b9f8ac17b2e9f5f817c35a13e43e59
DauphalXir <145732644+Keriils@users.noreply.github.com>
2025-04-01 10:15:28 +0800

    remove pollution effect

--- src/main/java/com/xir/NHUtilities/mixins/late/GregTech/ModifySomeConfigs.java
+++ src/main/java/com/xir/NHUtilities/mixins/late/GregTech/ModifySomeConfigs.java
@@ -5,6 +5,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.xir.NHUtilities.config.Config;
 
 import gregtech.GTMod;
@@ -22,6 +23,14 @@ public class ModifySomeConfigs {
         if (Config.enableAlwaysDisplayRecipeOwner) GTMod.gregtechproxy.mNEIRecipeOwner = true;
         if (Config.enableAlwaysDisplayWailaAverageNS) GTMod.gregtechproxy.wailaAverageNS = true;
         if (Config.enableAlwaysDisplayNEIOriginalVoltage) GTMod.gregtechproxy.mNEIOriginalVoltage = true;
+    }
+
+    @ModifyExpressionValue(
+        method = "loadConfig",
+        at = @At(value = "FIELD", target = "Lgregtech/common/pollution/PollutionConfig;pollutionPoisonLimit:I"),
+        require = 1)
+    private static int nhu$modification$2(int original) {
+        return Config.enableRemovePollutionEffectForPlayer ? Integer.MAX_VALUE : original;
     }
 
 }
