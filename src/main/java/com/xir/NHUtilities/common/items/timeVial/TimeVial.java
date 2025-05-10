e62f17387dd6840bb74a8a3c6754f49441f8ac72
DauphalXir <145732644+Keriils@users.noreply.github.com>
2025-05-10 15:18:34 +0800

    time vial optimization (#126)

--- src/main/java/com/xir/NHUtilities/common/items/timeVial/TimeVial.java
+++ src/main/java/com/xir/NHUtilities/common/items/timeVial/TimeVial.java
@@ -1,7 +1,9 @@
 package com.xir.NHUtilities.common.items.timeVial;
 
+import static com.xir.NHUtilities.common.entity.EntityTimeAccelerator.ACCELERATION_TICK;
 import static com.xir.NHUtilities.config.Config.defaultTimeVialVolumeValue;
 import static com.xir.NHUtilities.config.Config.enableLogInfo;
+import static com.xir.NHUtilities.config.Config.enableResetRemainingTime;
 import static com.xir.NHUtilities.config.Config.enableTimeAcceleratorBoost;
 import static com.xir.NHUtilities.config.Config.limitOneTimeVial;
 import static com.xir.NHUtilities.config.Config.timeVialDiscountValue;
@@ -39,12 +41,152 @@ public class TimeVial extends ItemBase {
     protected static final int MAX_ACCELERATION = enableTimeAcceleratorBoost ? 256 : 128;
     protected static final int NUMBER_EER = -846280; // ha,.... 114514
     protected int storedTimeTick = 0;
-    protected final double tHalfSize = 0.01D; // 实体一半的大小
+
+    protected static final double tHalfSize = 0.01D; // 实体一半的大小
+    protected static final String NBT_STORED_TICK = "storedTimeTick";
 
     public TimeVial() {
         super("TimeVial");
         setMaxStackSize(1);
         setTextureName("TimeVial/TimeVial");
+    }
+
+    @Override
+    public boolean onItemUseFirst(ItemStack stack, EntityPlayer player, @NotNull World world, int x, int y, int z,
+        int side, float hitX, float hitY, float hitZ) {
+
+        if (world.isRemote) return super.onItemUseFirst(stack, player, world, x, y, z, side, hitX, hitY, hitZ);
+
+        // 实体中心坐标
+        double targetPosX = x + 0.5D;
+        double targetPosY = y + 0.5D;
+        double targetPosZ = z + 0.5D;
+
+        // 计算碰撞箱大小
+        double minX = targetPosX - tHalfSize;
+        double minY = targetPosY - tHalfSize;
+        double minZ = targetPosZ - tHalfSize;
+        double maxX = targetPosX + tHalfSize;
+        double maxY = targetPosY + tHalfSize;
+        double maxZ = targetPosZ + tHalfSize;
+
+        // 获取碰撞箱对应实体
+        Optional<EntityTimeAccelerator> box = world
+            .getEntitiesWithinAABB(
+                EntityTimeAccelerator.class,
+                AxisAlignedBB.getBoundingBox(minX, minY, minZ, maxX, maxY, maxZ))
+            .stream()
+            .findFirst();
+
+        @NotNull
+        var eta = box.orElseGet(() -> new EntityTimeAccelerator(world, x, y, z));
+        if (box.isPresent()) {
+            if (player.isSneaking()) recyclingTime(stack, eta);
+            else applyNextAcceleration(stack, eta);
+        } else if (consumeTimeData(stack, (int) (TIME_INIT_RATE * 600 * timeVialDiscountValue))) {
+            // set the GregTechMachineMode
+            if (player.isSneaking()) eta.setGregTechMachineMode(false);
+            world.spawnEntityInWorld(eta);
+        }
+        etaInteract(eta, world, targetPosX, targetPosY, targetPosZ);
+        return true;
+    }
+
+    protected void applyNextAcceleration(ItemStack stack, EntityTimeAccelerator eta) {
+        var currentRate = eta.getTimeRate();
+        if (currentRate < MAX_ACCELERATION) {
+            var remained = currentRate * eta.getRemainingTime();
+            var nextRateTimeRequired = enableResetRemainingTime
+                ? (int) ((2 * currentRate * ACCELERATION_TICK - remained) * timeVialDiscountValue)
+                : (int) (remained * timeVialDiscountValue);
+            if (consumeTimeData(stack, nextRateTimeRequired)) {
+                eta.setTimeRate(currentRate * 2);
+                if (enableResetRemainingTime) eta.setRemainingTime(ACCELERATION_TICK);
+            }
+        }
+    }
+
+    protected static void recyclingTime(ItemStack stack, EntityTimeAccelerator eta) {
+        NBTTagCompound nbtTagCompound = stack.getTagCompound();
+        if (nbtTagCompound != null) {
+            nbtTagCompound.setInteger(
+                NBT_STORED_TICK,
+                nbtTagCompound.getInteger(NBT_STORED_TICK) + eta.getTimeRate() * eta.getRemainingTime());
+            stack.setTagCompound(nbtTagCompound);
+        }
+        eta.setDead();
+    }
+
+    protected void etaInteract(@NotNull EntityTimeAccelerator eta, World world, double targetPosX, double targetPosY,
+        double targetPosZ) {
+        int i = (int) (Math.log(eta.getTimeRate()) / Math.log(2)) - (enableTimeAcceleratorBoost ? 3 : 2);
+        // security considerations
+        if (i < 0 || i >= SOUND_ARRAY_F.length) i = 0;
+        world.playSoundEffect(
+            targetPosX,
+            targetPosY,
+            targetPosZ,
+            "note.harp",
+            defaultTimeVialVolumeValue,
+            SOUND_ARRAY_F[i]);
+        if (enableLogInfo) {
+            var remainingTime = eta.getRemainingTime();
+            LOG.info("xxxxx remainingTime: {} xxxxx", remainingTime);
+            LOG.info(
+                "An entity entityTimeAccelerator has been spawned ({}, {}, {}).",
+                targetPosX,
+                targetPosY,
+                targetPosZ);
+        }
+    }
+
+    protected boolean consumeTimeData(@NotNull ItemStack stack, int consumedTick) {
+        NBTTagCompound tagCompound = stack.getTagCompound();
+        int timeTick = tagCompound.getInteger(NBT_STORED_TICK);
+        if (timeTick >= consumedTick) {
+            tagCompound.setInteger(NBT_STORED_TICK, timeTick - consumedTick);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onUpdate(ItemStack stack, World worldIn, Entity playerIn, int slot, boolean isHeld) {
+        if (worldIn.isRemote) return;
+        if (!(playerIn instanceof EntityPlayer player)) return;
+        if (limitOneTimeVial && worldIn.getTotalWorldTime() % 600 == 0) mergeSameVialTime(player, stack);
+        NBTTagCompound nbtTagCompound = stack.getTagCompound();
+        if (nbtTagCompound == null) {
+            nbtTagCompound = new NBTTagCompound();
+            nbtTagCompound.setInteger(NBT_STORED_TICK, storedTimeTick);
+        } else if (worldIn.getTotalWorldTime() % 20 == 0) {
+            int t = nbtTagCompound.getInteger(NBT_STORED_TICK);
+            if (t == NUMBER_EER) return;
+            nbtTagCompound.setInteger(NBT_STORED_TICK, t + 20);
+        }
+        stack.setTagCompound(nbtTagCompound);
+    }
+
+    protected void mergeSameVialTime(@NotNull EntityPlayer player, ItemStack stack) {
+        for (ItemStack itemStack : player.inventory.mainInventory) {
+            if (itemStack != null && itemStack.getItem() == this && itemStack != stack) {
+                int thisTimeTick = stack.getTagCompound()
+                    .getInteger(NBT_STORED_TICK);
+                int otherTimeTick = itemStack.getTagCompound()
+                    .getInteger(NBT_STORED_TICK);
+                if (otherTimeTick >= 0 && thisTimeTick >= otherTimeTick) {
+                    stack.getTagCompound()
+                        .setInteger(NBT_STORED_TICK, thisTimeTick + otherTimeTick);
+                    itemStack.getTagCompound()
+                        .setInteger(NBT_STORED_TICK, NUMBER_EER);
+                }
+            }
+        }
+    }
+
+    @Override
+    public Entity createEntity(World world, Entity location, ItemStack itemstack) {
+        return new EntityImmortalItem(world, location, itemstack); // make this item Immortal
     }
 
     @Override
@@ -80,152 +222,10 @@ public class TimeVial extends ItemBase {
     protected void getInfoFromNBT(@NotNull ItemStack stack, List<String> list) {
         NBTTagCompound nbtTagCompound = stack.getTagCompound();
         if (nbtTagCompound == null) nbtTagCompound = new NBTTagCompound();
-        int storedTimeSeconds = nbtTagCompound.getInteger("storedTimeTick") / 20;
+        int storedTimeSeconds = nbtTagCompound.getInteger(NBT_STORED_TICK) / 20;
         int hours = storedTimeSeconds / 3600;
         int minutes = (storedTimeSeconds % 3600) / 60;
         int seconds = storedTimeSeconds % 60;
         list.add(I18n.format("text.TimeVial.tips", hours, minutes, seconds));
-    }
-
-    @Override
-    public boolean onItemUseFirst(ItemStack stack, EntityPlayer player, @NotNull World world, int x, int y, int z,
-        int side, float hitX, float hitY, float hitZ) {
-        if (!world.isRemote) {
-
-            // 实体中心坐标
-            double targetPosX = x + 0.5D;
-            double targetPosY = y + 0.5D;
-            double targetPosZ = z + 0.5D;
-
-            // 计算碰撞箱大小
-            double minX = targetPosX - tHalfSize;
-            double minY = targetPosY - tHalfSize;
-            double minZ = targetPosZ - tHalfSize;
-            double maxX = targetPosX + tHalfSize;
-            double maxY = targetPosY + tHalfSize;
-            double maxZ = targetPosZ + tHalfSize;
-
-            // 获取碰撞箱对应实体
-            Optional<EntityTimeAccelerator> box = world
-                .getEntitiesWithinAABB(
-                    EntityTimeAccelerator.class,
-                    AxisAlignedBB.getBoundingBox(minX, minY, minZ, maxX, maxY, maxZ))
-                .stream()
-                .findFirst();
-
-            if (box.isPresent()) {
-
-                EntityTimeAccelerator eta = box.get();
-
-                int currentRate = eta.getTimeRate();
-                int nextRateTimeRequired = (int) (currentRate * eta.getRemainingTime() * timeVialDiscountValue);
-
-                if (player.isSneaking()) { // recycling time
-                    NBTTagCompound nbtTagCompound = stack.getTagCompound();
-                    if (nbtTagCompound != null) {
-                        nbtTagCompound.setInteger(
-                            "storedTimeTick",
-                            nbtTagCompound.getInteger("storedTimeTick") + currentRate * eta.getRemainingTime());
-                        stack.setTagCompound(nbtTagCompound);
-                    }
-                    eta.setDead();
-                } else if (currentRate < MAX_ACCELERATION && shouldAndConsumeTimeData(stack, nextRateTimeRequired)) {
-                    eta.setTimeRate(currentRate * 2);
-                    etaInteract(eta, world, targetPosX, targetPosY, targetPosZ);
-                }
-            } else if (shouldAndConsumeTimeData(stack, TIME_INIT_RATE * 600)) {
-                EntityTimeAccelerator eta = new EntityTimeAccelerator(world, x, y, z);
-
-                // set the GregTechMachineMode
-                if (player.isSneaking()) eta.setGregTechMachineMode(false);
-
-                world.spawnEntityInWorld(eta);
-
-                etaInteract(eta, world, targetPosX, targetPosY, targetPosZ);
-            }
-            return true;
-        }
-        return false;
-    }
-
-    private void etaInteract(@NotNull EntityTimeAccelerator eta, World world, double targetPosX, double targetPosY,
-        double targetPosZ) {
-        this.playSoundForRateChange(world, targetPosX, targetPosY, targetPosZ, eta.getTimeRate());
-        if (enableLogInfo) {
-            this.debugInfo(targetPosX, targetPosY, targetPosZ, eta.getRemainingTime());
-        }
-    }
-
-    private void playSoundForRateChange(@NotNull World world, double targetPosX, double targetPosY, double targetPosZ,
-        int currentRate) {
-        int i = (int) (Math.log(currentRate) / Math.log(2)) - (enableTimeAcceleratorBoost ? 3 : 2);
-        // security considerations
-        if (i < 0 || i >= SOUND_ARRAY_F.length) i = 0;
-        world.playSoundEffect(
-            targetPosX,
-            targetPosY,
-            targetPosZ,
-            "note.harp",
-            defaultTimeVialVolumeValue,
-            SOUND_ARRAY_F[i]);
-    }
-
-    private void debugInfo(double targetPosX, double targetPosY, double targetPosZ, int remainingTime) {
-        LOG.info("xxxxx remainingTime: {} xxxxx", remainingTime);
-        LOG.info("An entity entityTimeAccelerator has been spawned ({}, {}, {}).", targetPosX, targetPosY, targetPosZ);
-
-    }
-
-    protected boolean shouldAndConsumeTimeData(@NotNull ItemStack stack, int consumedTick) {
-        int timeTick = stack.getTagCompound()
-            .getInteger("storedTimeTick");
-        if (timeTick >= consumedTick) {
-            stack.getTagCompound()
-                .setInteger("storedTimeTick", timeTick - consumedTick);
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public void onUpdate(ItemStack stack, World worldIn, Entity playerIn, int slot, boolean isHeld) {
-        if (worldIn.isRemote) return;
-        if (!(playerIn instanceof EntityPlayer player)) return;
-        if (limitOneTimeVial && worldIn.getTotalWorldTime() % 600 == 0) {
-            mergeSameVialTime(player, stack);
-        }
-        NBTTagCompound nbtTagCompound = stack.getTagCompound();
-        if (nbtTagCompound == null) {
-            nbtTagCompound = new NBTTagCompound();
-            nbtTagCompound.setInteger("storedTimeTick", storedTimeTick);
-            stack.setTagCompound(nbtTagCompound);
-        } else if (worldIn.getTotalWorldTime() % 20 == 0) {
-            int t = nbtTagCompound.getInteger("storedTimeTick");
-            if (t == NUMBER_EER) return;
-            nbtTagCompound.setInteger("storedTimeTick", t + 20);
-            stack.setTagCompound(nbtTagCompound);
-        }
-    }
-
-    protected void mergeSameVialTime(@NotNull EntityPlayer player, ItemStack stack) {
-        for (ItemStack itemStack : player.inventory.mainInventory) {
-            if (itemStack != null && itemStack.getItem() == this && itemStack != stack) {
-                int thisTimeTick = stack.getTagCompound()
-                    .getInteger("storedTimeTick");
-                int otherTimeTick = itemStack.getTagCompound()
-                    .getInteger("storedTimeTick");
-                if (otherTimeTick >= 0 && thisTimeTick >= otherTimeTick) {
-                    stack.getTagCompound()
-                        .setInteger("storedTimeTick", thisTimeTick + otherTimeTick);
-                    itemStack.getTagCompound()
-                        .setInteger("storedTimeTick", NUMBER_EER);
-                }
-            }
-        }
-    }
-
-    @Override
-    public Entity createEntity(World world, Entity location, ItemStack itemstack) {
-        return new EntityImmortalItem(world, location, itemstack); // make this item Immortal
     }
 }
